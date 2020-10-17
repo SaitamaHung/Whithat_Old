02a29f0a80f277567f4289f8ee62ac6a6dff4a5c
Rintim <therintim@gmail.com>
2020-10-17 18:10:30 +0800

    2020/10/17(ChiShyo) 実験可能

--- Source/config.ex
+++ Source/config.ex
@@ -1,4 +1,6 @@
 defmodule Whithat.Config do
-	def sessdata,do: "Put Your Sessdata Here"
+	def version,do: "Alpha 0.0.1"
+	def full_version,do: [build: 1]
+	def sessdata,do: "7c48c14f%2C1613485723%2Cb3235*81"
 	def ffmpeg,do: false
 end
