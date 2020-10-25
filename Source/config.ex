d7becb10e14b0f5090f08380c0e369d22e9a628e
Rintim <therintim@gmail.com>
2020-10-25 12:50:07 +0800

    Another Little

--- Source/config.ex
+++ Source/config.ex
@@ -9,10 +9,6 @@
 defmodule Whithat.Config do
 	def version,do: "Alpha 0.0.1"
 	def full_version,do: [build: 1]
-<<<<<<< HEAD
-	def sessdata,do: "7c48c14f%2C1613485723%2Cb3235*81"
-=======
 	def sessdata,do: "Put Your Sessdata Here"
->>>>>>> Development
 	def ffmpeg,do: false
 end
