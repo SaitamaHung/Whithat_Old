c4825ca1eaed405766b99cdcd6cb2683c68570f2
Rintim <therintim@gmail.com>
2020-10-31 22:21:53 +0800

    2020/10/31(Inchar) Add License

--- Library/video.ex
+++ Library/video.ex
@@ -1,5 +1,13 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# This File if From Theopse (Self@theopse.org)
+# Licensed under BSD-2-Caluse
+# File:	video.ex (Whithat/Library/video.ex)
+# Content:	Temporarily a templace
+# Copyright (c) 2020 Theopse Organization All rights reserved
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
 defmodule Whithat.Video do
-  @moduledoc """
+	@moduledoc """
 	A open library about Stream Media Website
 	"""
 end
