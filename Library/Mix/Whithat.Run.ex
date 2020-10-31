7eb2158fbe1c529c05087f999504d6f40b72a59b
Rintim <therintim@gmail.com>
2020-10-31 22:20:03 +0800

    2020/10/31(Inchar) Add License

--- Library/Mix/Whithat.Run.ex
+++ Library/Mix/Whithat.Run.ex
@@ -1,12 +1,19 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# This File if From Theopse (Self@theopse.org)
+# Licensed under BSD-2-Caluse
+# File:	Whithat.Run.ex (Whithat/Library/Mix/Tasks/Whithat.Run.ex)
+# Content:	To make the "mix whithat.run" enable
+# Copyright (c) 2020 Theopse Organization All rights reserved
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 defmodule Mix.Tasks.Whithat.Run do
-  use Mix.Task
+	use Mix.Task
 
-  @shortdoc "Run This Application"
-  @spec run(any) :: no_return()
-  def run(args) do
-    # This will start our application
-    Mix.Task.run("app.start")
-    Whithat.CLI.main(args)
-  end
+	@shortdoc "Run This Application"
+	@spec run(any) :: no_return()
+	def run(args) do
+		# This will start our application
+		Mix.Task.run("app.start")
+		Whithat.CLI.main(args)
+	end
 end
